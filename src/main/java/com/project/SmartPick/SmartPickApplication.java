c47fd20a09e68bb256f0f9762ecc49923139585a
Aldo Grabić <aldo.grabic99@gmail.com>
2023-12-24 12:23:34 +0100

    first commit

---
+++ src/main/java/com/project/SmartPick/SmartPickApplication.java
@@ -0,0 +1,13 @@
+package com.project.SmartPick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/SmartPick/SmartPickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.SmartPick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
