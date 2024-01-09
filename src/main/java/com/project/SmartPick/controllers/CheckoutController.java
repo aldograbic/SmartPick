62dad0537b8cd359d2f6f0db90aab098e2200eaa
Aldo Grabić <aldo.grabic99@gmail.com>
2024-01-09 15:49:46 +0100

    Popravljen prikaz proizvoda u checkoutu

--- src/main/java/com/project/SmartPick/controllers/CheckoutController.java
+++ src/main/java/com/project/SmartPick/controllers/CheckoutController.java
@@ -3,6 +3,9 @@ package com.project.SmartPick.controllers;
 import com.project.SmartPick.classes.user.User;
 import com.project.SmartPick.classes.user.UserRepository;
 import com.project.SmartPick.classes.order.OrderItem;
+import com.project.SmartPick.classes.order.OrderRepository;
+import com.project.SmartPick.classes.product.Product;
+import com.project.SmartPick.classes.product.ProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -10,6 +13,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.servlet.http.HttpSession;
 import java.math.BigDecimal;
@@ -20,6 +24,12 @@ public class CheckoutController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
 
     @GetMapping("/checkout")
     public String getCheckoutPage(HttpSession session, Model model) {
@@ -34,8 +44,20 @@ public class CheckoutController {
         
         @SuppressWarnings("unchecked")
         List<OrderItem> orderItems = (List<OrderItem>) session.getAttribute("orderItems");
+
+        for (OrderItem orderItem : orderItems) {
+            int productId = orderItem.getProductId();
+            Product product = productRepository.getProductByProductId(productId);
+            orderItem.setProduct(product);
+        }
         model.addAttribute("orderItems", orderItems);
 
         return "checkout";
+    }
+
+    @PostMapping("/order")
+    public String order() {
+
+        return "redirect:/";
     }
 }
